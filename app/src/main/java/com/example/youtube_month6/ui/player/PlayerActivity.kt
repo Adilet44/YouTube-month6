b4fdd99f52bf6cc670078c528cc342013a7abd6f
adilet44 <usupbekovadilet20@gmail.com>
2023-08-23 15:41:43 +0300

    player layout

---
+++ app/src/main/java/com/example/youtube_month6/ui/player/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.youtube_month6.ui.player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.youtube_month6.R
+
+class PlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
